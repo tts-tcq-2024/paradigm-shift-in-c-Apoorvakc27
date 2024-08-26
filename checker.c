fe0ddee87fe6b11edd35a21fb47e078d188690fd
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-08-26 15:55:16 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -6,10 +6,11 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
     int out_of_range = (temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) || (chargeRate > 0.8);
 
     // Print statements for each condition
+    /*
     printf("temperature %s\n", (temperature < 0 || temperature > 45) ? "out of range" : "in range");
     printf("soc %s\n", (soc < 20 || soc > 80) ? "out of range" : "in range");
     printf("chargeRate %s\n", (chargeRate > 0.8) ? "out of range" : "in range");
-
+    */
     // Return 0 if any condition is out of range, otherwise return 1
     return !out_of_range;
 }
