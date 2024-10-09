a51796b5d4aa725361c1178d547b6224774b9810
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-09 12:47:13 +0530

    Create range_check.h

---
+++ range_check.h
@@ -0,0 +1,10 @@
+#ifndef RANGE_CHECK_H
+#define RANGE_CHECK_H
+
+int is_value_in_range(float value, float min, float max);
+void check_warning(const char *paramName, float value, float min, float max, float tolerance);
+int check_temperature(float temperature);
+int check_soc(float soc);
+int check_chargeRate(float chargeRate);
+
+#endif // RANGE_CHECK_H
