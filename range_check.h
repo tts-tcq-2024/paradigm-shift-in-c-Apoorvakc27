fdb9e38d7437b29d99fbe031fac9b13d84e08657
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-09 15:42:45 +0530

    Update range_check.h

--- range_check.h
+++ range_check.h
@@ -2,11 +2,13 @@
 #define RANGE_CHECK_H
 
 int is_value_in_range(float value, float min, float max);
-void check_warning(const char *paramName, float value, float min, float max, float tolerance);
+void check_warning(const char *paramName, float value, float min, float max, float lowerTolerance, float upperTolerance);
 int check_temperature(float temperature);
 int check_soc(float soc);
 int check_chargeRate(float chargeRate);
 void check_error_or_warning(const char *paramName, float value, float min, float max, float lowerTolerance, float upperTolerance);
 int check_error(const char *paramName, float value, float min, float max);
+
+
 
 #endif // RANGE_CHECK_H
