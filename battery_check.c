da8e85010f18c95573d5e253e86740f883552aa9
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-09 12:47:56 +0530

    Create battery_check.c

---
+++ battery_check.c
@@ -0,0 +1,16 @@
+#include "battery_check.h"
+#include "range_check.h"
+
+// Function to evaluate the result of all checks
+int evaluate_checks(int tempCheck, int socCheck, int chargeRateCheck) {
+    return tempCheck * socCheck * chargeRateCheck;
+}
+
+// Combined function to check all parameters
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    int tempCheck = check_temperature(temperature);
+    int socCheck = check_soc(soc);
+    int chargeRateCheck = check_chargeRate(chargeRate);
+
+    return evaluate_checks(tempCheck, socCheck, chargeRateCheck);
+}
