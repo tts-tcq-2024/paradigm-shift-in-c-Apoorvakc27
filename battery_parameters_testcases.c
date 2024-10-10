0b10484f47d5697bccf916c35baca5584c2d0e3b
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-10 16:08:47 +0530

    Update battery_parameters_testcases.c

--- battery_parameters_testcases.c
+++ battery_parameters_testcases.c
@@ -1,23 +1,27 @@
 #include <assert.h>
+#include <stdbool.h>  // Include for boolean types
 #include "battery_check.h"
-#include "battery_parameters_testcases.h"
 
-void runAllTests() {
+bool runAllTests() {
+    bool allTestsPassed = true;
+
     // Temperature Tests
-    assert(batteryIsOk(25, 70, 0.7)); // All values within range
-    assert(!batteryIsOk(50, 70, 0.7)); // Temperature out of range
-    assert(batteryIsOk(41, 70, 0.7)); // Temperature warning (approaching upper limit)
-    assert(batteryIsOk(3, 70, 0.7)); // Temperature warning (approaching lower limit)
+    allTestsPassed &= batteryIsOk(25, 70, 0.7);   // All values within range
+    allTestsPassed &= !batteryIsOk(50, 70, 0.7);  // Temperature out of range
+    allTestsPassed &= batteryIsOk(41, 70, 0.7);   // Temperature warning (approaching upper limit)
+    allTestsPassed &= batteryIsOk(3, 70, 0.7);    // Temperature warning (approaching lower limit)
 
     // SOC Tests
-    assert(batteryIsOk(25, 70, 0.7)); // All values within range
-    assert(!batteryIsOk(25, 85, 0.7)); // SOC out of range (above maximum)
-    assert(!batteryIsOk(25, 15, 0.7)); // SOC out of range (below minimum)
-    assert(batteryIsOk(25, 76, 0.7)); // SOC warning (approaching upper limit)
-    assert(batteryIsOk(25, 24, 0.7)); // SOC warning (approaching lower limit)
+    allTestsPassed &= batteryIsOk(25, 70, 0.7);   // All values within range
+    allTestsPassed &= !batteryIsOk(25, 85, 0.7);  // SOC out of range (above maximum)
+    allTestsPassed &= !batteryIsOk(25, 15, 0.7);  // SOC out of range (below minimum)
+    allTestsPassed &= batteryIsOk(25, 76, 0.7);   // SOC warning (approaching upper limit)
+    allTestsPassed &= batteryIsOk(25, 24, 0.7);   // SOC warning (approaching lower limit)
 
     // Charge Rate Tests
-    assert(batteryIsOk(25, 70, 0.7)); // All values within range
-    assert(!batteryIsOk(25, 70, 0.9)); // Charge rate out of range
-    assert(batteryIsOk(25, 70, 0.75)); // Charge rate warning (approaching upper limit)
+    allTestsPassed &= batteryIsOk(25, 70, 0.7);   // All values within range
+    allTestsPassed &= !batteryIsOk(25, 70, 0.9);  // Charge rate out of range
+    allTestsPassed &= batteryIsOk(25, 70, 0.75);  // Charge rate warning (approaching upper limit)
+
+    return allTestsPassed;
 }
