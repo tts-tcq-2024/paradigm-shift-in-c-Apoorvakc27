316d0c052ab9908f71648d094b5453dd06173f27
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-10 15:29:33 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>  // Include for boolean types
 #include "battery_check.h"
 #include "range_check.h"
 
 // Function to evaluate the result of all checks
-int evaluate_checks(int tempCheck, int socCheck, int chargeRateCheck) {
-    return tempCheck * socCheck * chargeRateCheck;
+bool evaluate_checks(bool tempCheck, bool socCheck, bool chargeRateCheck) {
+    return tempCheck && socCheck && chargeRateCheck;
 }
 
 // Combined function to check all parameters
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    int tempCheck = check_temperature(temperature);
-    int socCheck = check_soc(soc);
-    int chargeRateCheck = check_chargeRate(chargeRate);
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+    bool tempCheck = check_temperature(temperature);
+    bool socCheck = check_soc(soc);
+    bool chargeRateCheck = check_chargeRate(chargeRate);
 
     return evaluate_checks(tempCheck, socCheck, chargeRateCheck);
 }
