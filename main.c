8a8a0fc037d174f3647f34cccd0de6cca29e448a
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-10 16:11:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "battery_parameters_testcases.h"
 
 int main() {
-    runAllTests();
-    return 0;
+    bool allTestsPassed = runAllTests();
+    return allTestsPassed ? 0 : 1; // Return 0 if all tests pass, 1 otherwise
 }
+
