e38e2f8c5d315686125c0824501c8b0f71a7a2eb
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-10-10 15:57:51 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "battery_check.h"
 #include "range_check.h"
+#include "soc_check.h"
+#include "temperature_check.h"
+#include "charge_rate_check.h"
+
+
 
 // Function to evaluate the result of all checks
 bool evaluate_checks(bool tempCheck, bool socCheck, bool chargeRateCheck) {
